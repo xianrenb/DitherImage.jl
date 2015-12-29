c96f461648098b0537fc993a4cf6971b1ac107f2
Peter Lee <xianrenb@gmail.com>
2015-12-29 19:43:01 +0800

    changed DitherImage.jl

--- src/DitherImage.jl
+++ src/DitherImage.jl
@@ -1,5 +1,53 @@
 module DitherImage
 
-# package code goes here
+export ditherimage
 
-end # module
+function ditherimage(img::Array{Float64,2}, t::Task)
+    width = size(img, 1)
+    height = size(img, 2)
+    tmp = copy(img)
+    out = zeros(Float64, width, height)
+
+    for i = 1:height
+        for j = 1:width
+            weights = consume(t)
+            weights_sum = sum(weights)
+
+            if tmp[j, i] >= 0.5
+                out[j, i] = 1.0
+            else
+                out[j, i] = 0.0
+            end
+
+            diff = tmp[j, i] - out[j, i]
+
+            if j < width
+                tmp[j + 1, i] += diff * weights[1] / weights_sum
+            end
+
+            if i < height
+                if j > 1
+                    tmp[j - 1, i + 1] += diff * weights[2] / weights_sum
+                end
+
+                tmp[j, i + 1] += diff * weights[3] / weights_sum
+
+                if j < width
+                    tmp[j + 1, i + 1] += diff * weights[4] / weights_sum
+                end
+            end
+        end
+    end
+
+    out
+end
+
+function ditherimage(img::Array{Float64,2})
+    ditherimage(img, @task begin
+        while true
+            produce([7, 3, 5, 1])
+        end
+    end)
+end
+
+end #module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,21 @@
 using DitherImage
 using Base.Test
+using Images
+using TestImages
 
-# write your own tests here
-@test 1 == 1
+img = testimage("lena_gray_256")
+imA = convert(Array{Float64, 2}, data(img))
+imA_out = ditherimage(imA)
+img_expected = load("lena_dither.png")
+imA_expected = convert(Array{Float64, 2}, data(img_expected))
+
+w = size(imA_out, 1)
+h = size(imA_out, 2)
+@test w == size(imA_expected, 1)
+@test h == size(imA_expected, 2)
+
+for i = 1:h
+    for j = 1:w
+        @test imA_out[w, h] == imA_expected[w, h]
+    end
+end
